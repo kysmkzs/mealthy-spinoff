96635c6f8bbcd7d62dce4b398266b50b5e191e0d
kysmkzs1 <masa@mealthy.me>
2015-09-24 09:35:29 +0000

    add post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   end
   
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   private
